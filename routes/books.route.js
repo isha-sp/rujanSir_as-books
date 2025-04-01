f5a47825bcc2d219661f39008ee02c2318691bf8
Sp-ishab <isha_bansal@softprodigy.com>
2025-04-01 19:11:02 +0530

    get reviews

--- routes/books.route.js
+++ routes/books.route.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const {createBook, getBooks, updateBook, deleteBook} = require("../controllers/books.controller");
+const {createBook, getBooks, updateBook, deleteBook, getBookReviews} = require("../controllers/books.controller");
 
 router.route("/").post(createBook).get(getBooks);
-router.route("/:bookid").put(updateBook).delete(deleteBook);
+router.route("/:bookid").patch(updateBook).delete(deleteBook).get(getBookReviews);
 
 module.exports = router;
